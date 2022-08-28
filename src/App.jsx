f59df6727beaf834972729d6f7cce60d253981ee
andres lopez <andresfelipelg@gmail.com>
2022-08-28 11:46:01 -0500

    se guardo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Crypto from './components/Crypto'
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <div className="container">
+      <h1 className="titulo">React Crypto</h1>
+      <Crypto />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Crypto.jsx
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import {useState,useEffect} from 'react'
+
+const Crypto = () => {
+
+    const [search, setSearch] = useState('');
+    const [cryptos, setCryptos] = useState([]);
+
+    const endpoint ='https://api.coingecko.com/api/v3/coins'
+
+    const showData = () => {
+        axios.get(endpoint)
+        .then((res) => {
+            console.log(res.data);
+            setCryptos(res.data);
+        })
+   
+   
+    }
+    useEffect(() => {
+      
+      showData() 
+    }, []);
+    
+    const searcher = (e) => {
+        setSearch(e.target.value);
+    }
+    
+    const results = !search ? cryptos : 
+    cryptos.filter(crypto => crypto.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+
+  return (
+    <>
+    
+    <input type="text" 
+     value ={search}
+     placeholder="Ingresa la crypto moneda a consultar"
+     onChange={searcher}
+    className="form-control mb-4"
+    />
+
+    <table className="table table-responsive table-hover table-dark">
+          <thead> 
+             <tr>
+             <th>Ranking</th>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Price 24h</th>
+            </tr>
+        </thead>
+        <tbody>
+         { results.map( (result)=> (
+                <tr key={result.id}>
+                    <td>{result.market_data.market_cap_rank}</td>
+                    <td><small><img src={result.image.small}/> {result.name} </small></td>
+                    <td>{result.symbol.toUpperCase()}</td>
+                    <td>{result.market_data.current_price.bmd.toFixed(2)}</td>
+                    <td>
+                        { result.market_data.price_change_percentage_24h < 0 ? (
+                          <span className='badge bg-danger'>{result.market_data.price_change_percentage_24h}</span>  
+                        ):(
+                          <span className='badge bg-success'>{result.market_data.price_change_percentage_24h}</span>    
+                        ) }
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+    
+    </>
+  )
+}
+
+export default Crypto
